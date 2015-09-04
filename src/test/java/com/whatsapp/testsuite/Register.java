580e52c153b28ad434e13c2ff6c06666f566a218
viet.ch <viet.ch2612@gmail.com>
2015-09-04 17:00:50 +0700

    Update testcase

--- src/test/java/com/whatsapp/testsuite/Register.java
+++ src/test/java/com/whatsapp/testsuite/Register.java
@@ -1,7 +1,9 @@
 package com.whatsapp.testsuite;
 
-import com.whatsapp.service.SetupTest;
+import com.whatsapp.resource_id.RegisterId;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -10,6 +12,9 @@ import org.testng.annotations.Test;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+
+import static org.testng.AssertJUnit.assertTrue;
 
 /**
  * Created by viet.ch on 9/4/2015.
@@ -28,25 +33,59 @@ public class Register {
     File app = new File("src/resources/com.whatsapp.app.apk");
 
     @BeforeTest
-    private void beforeTest () throws MalformedURLException {
+    private void beforeTest() throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("deviceName","Android Emulator");
-        caps.setCapability("platform","ANDROID");
+        caps.setCapability("deviceName", "Android Emulator");
+        caps.setCapability("platform", "ANDROID");
         caps.setCapability("platformVersion", "5.0");
-        caps.setCapability("app",app);
-        driver = new AndroidDriver(new URL(URL),caps);
+        caps.setCapability("app", app);
+        driver = new AndroidDriver(new URL(URL), caps);
     }
 
     //Quit driver after test
     @AfterTest
-    private void afterTest () {
+    private void afterTest() {
         driver.quit();
     }
 
     //Run your test case
     @Test
     public void testRegister() {
-        System.out.println("Completed");
-    }
 
+        //Get alert element
+        WebElement alertTitle = driver.findElement(By.id(RegisterId.TITLE_ALERT));
+
+        //Check alert is displayed
+        if (alertTitle.isDisplayed()) {
+
+            //Click on OK button
+            System.out.println("Click on OK");
+            WebElement btnOK = driver.findElement(By.id(RegisterId.BTN_OK));
+            btnOK.click();
+
+            //Click to Agree and continue
+            System.out.println("Click on Agree");
+            WebElement btnAgree = driver.findElement(By.id(RegisterId.BTN_ACCEPT_AND_CONTINUE));
+            btnAgree.click();
+
+            //Select country: Vietnam
+            System.out.println("Click on Country");
+            WebElement btnCountry = driver.findElement(By.id(RegisterId.BTN_SELECT_COUNTRY));
+            btnCountry.click();
+
+
+            //Search textbox SEARCH, sendkeys: Vietnam and Select
+            System.out.println("Select Vietnam");
+            driver.findElementById(RegisterId.TEXT_SEARCH).sendKeys("Vietnam");
+            List<WebElement> elements = driver.findElements(By.id(RegisterId.TXT_COUNTRY_NAME_ENG));
+            for (WebElement element : elements) {
+                if (element.getText().equals("Vietnam")) {
+                    element.click();
+                } else {
+                    assertTrue(false);
+                }
+            }
+
+        }
+    }
 }
