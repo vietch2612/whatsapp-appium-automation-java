2d22c3cb5dcbce1897a87e8e999a1d767cd5cdd1
viet.ch <viet.ch2612@gmail.com>
2015-09-04 18:36:02 +0700

    Update testcase

--- src/test/java/com/whatsapp/testsuite/Register.java
+++ src/test/java/com/whatsapp/testsuite/Register.java
@@ -1,19 +1,21 @@
 package com.whatsapp.testsuite;
 
-import com.whatsapp.resource_id.RegisterId;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.UncheckedIOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
+import static com.whatsapp.resource_id.RegisterId.*;
 import static org.testng.AssertJUnit.assertTrue;
 
 /**
@@ -21,14 +23,12 @@ import static org.testng.AssertJUnit.assertTrue;
  */
 public class Register {
 
-    //Init appium android driver, if iOS you can call IosDriver
-    AndroidDriver driver;
-
     //Url of appium server.
     //How to find? here
     //Http://3.bp.blogspot.com/-vlxM0YgXspw/UvYS6kOZDdI/AAAAAAAAAiU/3JXuNp8AOPc/s1600/Appium.jpg
     static String URL = "http://127.0.0.1:4723/wd/hub";
-
+    //Init appium android driver, if iOS you can call IosDriver
+    AndroidDriver driver;
     //Get apk app location
     File app = new File("src/resources/com.whatsapp.app.apk");
 
@@ -50,42 +50,58 @@ public class Register {
 
     //Run your test case
     @Test
-    public void testRegister() {
-
-        //Get alert element
-        WebElement alertTitle = driver.findElement(By.id(RegisterId.TITLE_ALERT));
-
-        //Check alert is displayed
-        if (alertTitle.isDisplayed()) {
-
-            //Click on OK button
-            System.out.println("Click on OK");
-            WebElement btnOK = driver.findElement(By.id(RegisterId.BTN_OK));
-            btnOK.click();
-
-            //Click to Agree and continue
-            System.out.println("Click on Agree");
-            WebElement btnAgree = driver.findElement(By.id(RegisterId.BTN_ACCEPT_AND_CONTINUE));
-            btnAgree.click();
-
-            //Select country: Vietnam
-            System.out.println("Click on Country");
-            WebElement btnCountry = driver.findElement(By.id(RegisterId.BTN_SELECT_COUNTRY));
-            btnCountry.click();
-
-
-            //Search textbox SEARCH, sendkeys: Vietnam and Select
-            System.out.println("Select Vietnam");
-            driver.findElementById(RegisterId.TEXT_SEARCH).sendKeys("Vietnam");
-            List<WebElement> elements = driver.findElements(By.id(RegisterId.TXT_COUNTRY_NAME_ENG));
-            for (WebElement element : elements) {
-                if (element.getText().equals("Vietnam")) {
-                    element.click();
-                } else {
-                    assertTrue(false);
-                }
+    public void checkAlertPopup() {
+        try {
+            //Check alert is displayed
+            if (driver.findElementById(TITLE_ALERT).isDisplayed()) {
+                //Click on OK button
+                clickOn(BTN_OK);
             }
-
+        } catch (NullPointerException e) {
+            e.printStackTrace();
         }
     }
+
+    @Test(dependsOnMethods = {"checkAlertPopup"})
+    public void clickOnAgree() {
+        //Click to Agree and continue
+        clickOn(BTN_ACCEPT_AND_CONTINUE);
+    }
+
+    @Test(dependsOnMethods = {"clickOnAgree"}, dataProvider = "countryNameList")
+    public void selectCountry(String countryCode, String countryNameEnglish) throws UncheckedIOException{
+        //Select country
+        clickOn(BTN_SELECT_COUNTRY);
+
+        //Search Vietnam and select
+        sendKeys(TEXT_SEARCH, countryCode);
+        List<WebElement> elements = driver.findElementsById(TXT_COUNTRY_NAME_ENG);
+        for (WebElement element : elements) {
+            if (element.getText().equals(countryNameEnglish)) {
+                element.click();
+            } else {
+                assertTrue(false);
+            }
+        }
+    }
+
+    //Some country list
+    @DataProvider(name = "countryNameList")
+    public Object[][] listCountryName() {
+        return new Object[][]{
+                {"84", "Vietnam"},
+                {"379", "Vatican City"},
+                {"55", "Brazil"},
+                {"52", "Mexico"}
+        };
+    }
+
+    public void clickOn(String id) {
+        driver.findElement(By.id(id)).click();
+    }
+
+    public void sendKeys(String id, String keys) {
+        driver.findElement(By.id(id)).sendKeys(keys);
+    }
+
 }
